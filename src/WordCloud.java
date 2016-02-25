9de9bd1f5665752449c42fc9ec8564737ba73e01
Nick Lockett <NickS.Lockett@gmail.com>
2016-02-25 17:01:11 -0500

    nsl8 and klo14

--- src/WordCloud.java
+++ src/WordCloud.java
@@ -87,6 +87,7 @@ public class WordCloud {
                                // sort from most frequent to least
                                // TODO: add secondary comparison alphabetically based on word
                                .sorted(Comparator.comparing(Entry<String, Long>::getValue).reversed())
+                               .sorted(Comparator.comparing(Entry<String, Long>::getKey))
                                // keep only the top ones
                                .limit(numWordsToKeep)
                                // convert frequencies into groups (Entry is immutable, so create a new one)
@@ -117,9 +118,12 @@ public class WordCloud {
     private static List<String> readWords (Scanner input,
                                            UnaryOperator<String> xform,
                                            Predicate<String> select) {
+        
         List<String> contents = Arrays.stream(input.useDelimiter(END_OF_FILE).next().split(WHITESPACE))
                                       // TODO: add map and filter calls using parameters
-                                      .collect(Collectors.toList());
+                                      .map(w -> xform.apply(w))
+                                      .filter(select)
+                                      .collect(Collectors.toList());                
         input.close();
         return contents;
     }
