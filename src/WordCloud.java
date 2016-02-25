6277bbf0d145a855c95754e9697be43a3787190d
Frank Wang <fraankwang@gmail.com>
2016-02-25 17:04:38 -0500

    finished

--- src/WordCloud.java
+++ src/WordCloud.java
@@ -86,7 +86,9 @@ public class WordCloud {
         myTagWords = myTagWords.stream()
                                // sort from most frequent to least
                                // TODO: add secondary comparison alphabetically based on word
-                               .sorted(Comparator.comparing(Entry<String, Long>::getValue).reversed())
+								.sorted(Comparator.comparing(Entry<String, Long>::getValue).reversed()
+								.thenComparing(Comparator.comparing(Entry<String, Long>::getKey)))
+
                                // keep only the top ones
                                .limit(numWordsToKeep)
                                // convert frequencies into groups (Entry is immutable, so create a new one)
@@ -118,7 +120,8 @@ public class WordCloud {
                                            UnaryOperator<String> xform,
                                            Predicate<String> select) {
         List<String> contents = Arrays.stream(input.useDelimiter(END_OF_FILE).next().split(WHITESPACE))
-                                      // TODO: add map and filter calls using parameters
+        							  .filter(select)
+                                      .map(w -> xform.apply(w))
                                       .collect(Collectors.toList());
         input.close();
         return contents;
